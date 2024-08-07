bde560093fd5cb2a3e9131ff93ab38848cce3526
Jeong-su Shin <jeshin@cx1r3s3.42seoul.kr>
2024-08-07 18:59:16 +0900

    add cpp example folder

---
+++ CppForC/03_nameMangling/03_nameMangling.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void testFunc()
+{
+	cout << "::TestFunc()" << endl;
+}
+
+namespace Test
+{
+	void testFunc()
+	{
+		cout << "Test::TestFunc()" << endl;
+	}
+}
+
+namespace MyData
+{
+	void testFunc()
+	{
+		cout << "MyData::TestFunc()" << endl;
+	}
+}
+
+int main()
+{
+	testFunc();
+	::testFunc();
+	Test::testFunc();
+	MyData::testFunc();
+
+	return 0;
+}
